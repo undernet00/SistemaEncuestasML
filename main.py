2ffbeff28dbaccd0078899b3166d10ec78e2bab3
undernet00 <rivero.info@gmail.com>
2021-12-27 08:17:35 -0300

    El WS devuelve JSON con lista de etiquetas.
    El WS devuelve JSON con lista de etiquetas.

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 from flask import Flask
 from flask_restful import Api, Resource
-import spacy 
+import spacy
+import json
 
 
 app = Flask(__name__)
 api = Api(app)
 
+
 class Modelo(Resource):
     def get(self, frase):
-        
+
         model_path = 'model-best'
         model = spacy.load(model_path)
-        
+        labels = []
         doc = model(frase).doc
+
         for ent in doc.ents:
-            print(ent)
-            print(ent.label_)
-
-        return {"data" : frase}
-
-
-
+            #print(ent)
+            #print(ent.label_)
+            labels.append(ent.label_)
+            
+        return {"labels": (json.dumps(labels))}
 
 
 api.add_resource(Modelo, "/modelo/<string:frase>/")
 if __name__ == "__main__":
     app.run(debug=True)
-
