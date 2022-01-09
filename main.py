2c6cdd65f495b87f260cdb334fc7c3cc427c221a
undernet00 <rivero.info@gmail.com>
2022-01-09 12:56:19 -0300

    Nuevo retorno
    Ahora se retorna una lista de strings con las etiquetas.

--- main.py
+++ main.py
@@ -6,24 +6,22 @@ import json
 
 app = Flask(__name__)
 api = Api(app)
+model = spacy.load('model-best')
 
 
 class Modelo(Resource):
     def get(self, frase):
-
-        model_path = 'model-best'
-        model = spacy.load(model_path)
         labels = []
         doc = model(frase).doc
 
         for ent in doc.ents:
-            #print(ent)
-            #print(ent.label_)
             labels.append(ent.label_)
-            
-        return {"labels": (json.dumps(labels))}
 
+        print(labels)
+
+        return labels
+        
 
 api.add_resource(Modelo, "/modelo/<string:frase>/")
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
--- test.py
+++ test.py
@@ -1,12 +1,19 @@
 import requests
 
+#BASE = "http://190.64.90.147:1000/"
 BASE = "http://127.0.0.1:5000/"
+#BASE = "https://modelws.azurewebsites.net:5000/"
 
-FRASE = "La comida estaba muy rica. Especialmente el pollo. No así la milanesa. Creo que vamos a volver. Gracias por la buena atención.}"
-#FRASE = "frase"
+FRASE = "Me gustaron los huevos rotos. La verdad que la atención fue muy buena. Pero el precio me pareció un poco caro. Volvería."
+
+print ("--")
+print ("Frase:" + FRASE)
 
 response = requests.get(BASE + "modelo/" + FRASE)
 
-print (BASE + "modelo/" + FRASE)
+#print (BASE + "modelo/" + FRASE)
 
-print(response.json())
+print("Respuesta:" + str(response.text))
+
+
+print ("--")
