bc41bab59754d3f58d33084aac2724d182959be1
undernet00 <rivero.info@gmail.com>
2022-02-13 14:21:53 -0300

    Ahora acepta POST con JSON en body.
    Nueva versión del WS. Ahora acepta el texto de los comentarios como JSON en el body.

--- main.py
+++ main.py
@@ -1,27 +1,46 @@
-from flask import Flask
+from distutils.log import debug
+from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 import spacy
 import json
 
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 api = Api(app)
 model = spacy.load('model-best')
 
 
-class Modelo(Resource):
-    def get(self, frase):
+class Modelo():
+    def get(frase):
         labels = []
         doc = model(frase).doc
-
+        
         for ent in doc.ents:
             labels.append(ent.label_)
+            #print(ent)
 
-        print(labels)
-
+        #print(labels)
         return labels
         
+# ...
+@app.route('/modelo', methods=['POST'])
+def procesa_json():
+    content_type = request.headers.get('Content-Type')
+    #print("content_type: " + content_type)
+    #print("request.charset: " + request.charset)
+    
+    if (content_type == 'application/json'):
+        #Acción
+        mensaje = request.json['mensaje']
+        analisis = Modelo.get(mensaje)
+        jeison = jsonify(analisis)
+        return jeison
+        
 
-api.add_resource(Modelo, "/modelo/<string:frase>/")
+        return jsonify(datos)
+    else:
+        return 'Content-Type not supported!'
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0') 
